8f207dc5fcb766255a14d2b62b48c9d5a45910f2
ShredsAI <contact@shreds.ai>
2025-08-18 20:43:29 +0200

    Generating Unit Tests number: 7

--- src/main/java/ai/shreds/OrderShredApplication.java
+++ src/main/java/ai/shreds/OrderShredApplication.java
@@ -2,13 +2,19 @@ package ai.shreds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ai.shreds.infrastructure.repositories")
+@EntityScan(basePackages = "ai.shreds.domain.entities")
 @EnableJms
 @EnableRetry
+@EnableAsync
 @EnableTransactionManagement
 public class OrderShredApplication {
 
